c6fedbdea7f43e5565ae8292ece1cd89be311733
himavarshini28 <himavarshinimahapatruni@gmail.com>
2025-05-20 16:55:22 +0530

    property routes added

--- backend/routes/property.js
+++ backend/routes/property.js
@@ -3,8 +3,45 @@ import express from "express";
 import {propertyModel} from "../db.js";
 
 const propertyRouter = express.Router();
-propertyRouter.post("/addProperty",async(req,res)=>{})
-propertyRouter.post("/getProperties",async(req,res)=>{})
-propertyRouter.post("/getProperty",async(req,res)=>{})
+
+propertyRouter.post("/get-properties",async(req,res)=>{
+    try{
+        const properties = await propertyModel.find({});
+        res.status(200).json({
+            message:"fetched all properties",
+            properties,
+        })
+    }
+    catch(error)
+    {
+        res.status(500).json({
+            message:"error fetching properties",
+            error:error.message
+        })
+    }
+})
+propertyRouter.post("/get-property",async(req,res)=>{
+    const {propertyId}=req.body;
+    try{
+        const property = await propertyModel.findOne({_id:propertyId})
+        if(!property)
+        {
+            res.status(404).json({
+                message:"property not found"
+            })
+        }
+        res.status(200).json({
+            message:"property found",
+            property
+        })
+    }
+    catch(error)
+    {
+        res.status(500).json({
+            message:"error while fetching property",
+            error:error.message
+        })
+    }
+})
 
 export default propertyRouter;
