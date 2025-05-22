5a23661fe969862a4b9ffecca93bedb0f3646b39
himavarshini28 <himavarshinimahapatruni@gmail.com>
2025-05-22 21:36:36 +0530

    update

--- frontend/src/pages/Allproperties.jsx
+++ frontend/src/pages/Allproperties.jsx
@@ -8,7 +8,7 @@ const Allproperties = () => {
   useEffect(() => {
     const fetchProperties = async () => {
       try {
-        const response = await fetch('http://localhost:3000/api/property/v1/get-properties');
+        const response = await fetch('https://rent-it-zv5s.vercel.app/api/property/v1/get-properties');
         const data = await response.json();
         setProperties(data);
       } catch (error) {
